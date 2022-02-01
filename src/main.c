c43fc775643b675bf02e23f8ff7248dc4a55647b
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-02-01 13:12:52 -0500

    fix +, -, x operations

--- src/main.c
+++ src/main.c
@@ -22,9 +22,9 @@ int main (int argc, char **argv)
     {
         sscanf (argv[1], "%d", &arg1);
         sscanf (argv[3], "%d", &arg2);
-        if (argv[2] == '+') printf ("%d\n", arg1 + arg2);
-        if (argv[2] == '-') printf ("%d\n", arg1 - arg2);
-        if (argv[2] == 'x') printf ("%d\n", arg1 * arg2);
+        if (*argv[2] == '+') printf ("%d\n", arg1 + arg2);
+        if (*argv[2] == '-') printf ("%d\n", arg1 - arg2);
+        if (*argv[2] == 'x') printf ("%d\n", arg1 * arg2);
         if (*argv[2] == '/') printf ("%d\n", arg1 / arg2);
     }
     return 0;
